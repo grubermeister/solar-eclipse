0be1ba55ec93e9b445c1af58069e9337788d4025
ArcaneSunku <t4hnner@gmail.com>
2024-05-16 23:04:24 -0500

    Started reworking the server to be Terminal only.

--- server/src/git/eclipse/server/EclipseServer.java
+++ server/src/git/eclipse/server/EclipseServer.java
@@ -1,78 +1,92 @@
 package git.eclipse.server;
 
 import git.eclipse.core.network.ServerData;
-import git.eclipse.core.network.ServerHandler;
-import git.eclipse.core.swing.Console;
-import git.eclipse.core.swing.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EclipseServer implements Runnable {
 
     private final ServerData m_Data;
     private final Thread m_Thread;
-    private final Server m_Frame;
 
-    private ServerHandler m_Server;
     private volatile boolean m_Running;
 
     public EclipseServer(ServerData data) {
         m_Data = data;
         m_Running = false;
-        m_Frame = new Server(data.toString(), data.MotD);
-        m_Thread = new Thread(this, "Main_Thread");
+
+        m_Thread = new Thread(this, "Server_Thread");
     }
 
     public synchronized void start() {
         if(m_Running) return;
 
-        m_Server = new ServerHandler(m_Data.IP.getHostName(), m_Data.Port);
-        m_Server.start();
-
         m_Running = true;
         m_Thread.start();
     }
 
-    public synchronized void stop() {
-        if(!m_Running) return;
-        m_Running = false;
-    }
+    @Override
+    public void run() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    private void process() {
-        Console console = m_Frame.getConsoleTab();
-    }
+        while(m_Running) {
+            try {
+                String line = reader.readLine();
+                if(line == null) continue; // Don't want to crash, but also want nothing to happen
 
-    private void dispose() {
-        if(m_Running) m_Running = false;
+                boolean isCmd = line.charAt(0) == '/';
+                if(isCmd) { // If we pass a command, process it
+                    handleCommand(line.substring(1));
+                } else { // Otherwise treat as /say ...
+                    // For now, we simply echo it.
+                    System.out.println(line);
+                }
+
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+
         try {
-            if(!m_Frame.isClosing())
-                m_Frame.dispose();
-
-            if(m_Server.isRunning())
-                m_Server.stop();
-
+            reader.close();
             m_Thread.join(1);
-        } catch (InterruptedException e) {
-            System.err.println(e.getMessage());
+        } catch (InterruptedException | IOException e) {
+            System.out.println("Failed to join thread: \n" + e.getMessage());
             System.exit(-1);
         }
     }
 
-    @Override
-    public void run() {
-        m_Frame.setupFrame();
-        m_Frame.createUIComponents();
+    private void handleCommand(String input) {
+        String cmd = input;
+        List<String> arguments = new ArrayList<>();
+        boolean hasArgs = input.indexOf(' ') != -1;
+        if(hasArgs) {
+            String args = input.substring(input.indexOf(' ') + 1);
+            cmd = input.substring(0, input.indexOf(' '));
 
-        Console console = m_Frame.getConsoleTab();
-        console.pushMessage(m_Data.MotD);
-
-        while(m_Running) {
-            if(m_Frame.isClosing() || console.shouldClose()) {
-                stop();
-                continue;
+            while(!args.isEmpty()) {
+                if(args.indexOf(' ') != -1) {
+                    String curr = args.substring(0, args.indexOf(' '));
+                    arguments.add(curr);
+                    args = args.substring(args.indexOf(' ') + 1);
+                } else {
+                    arguments.add(args);
+                    args = "";
+                }
             }
-
-            process();
         }
 
-        dispose();
+        if(cmd.equalsIgnoreCase("exit")) {
+            m_Running = false;
+        } else if(cmd.equalsIgnoreCase("test")) {
+            System.out.println("Testing!");
+        }
+
+        if(!arguments.isEmpty())
+            arguments.clear();
     }
 }
