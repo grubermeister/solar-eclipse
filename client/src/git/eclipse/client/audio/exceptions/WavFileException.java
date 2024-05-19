df8944555350a6748039f20320010404a10fec06
ArcaneSunku <t4hnner@gmail.com>
2024-05-19 14:53:28 -0500

    Added classes for using WAV files.

---
+++ client/src/git/eclipse/client/audio/exceptions/WavFileException.java
@@ -0,0 +1,24 @@
+package git.eclipse.client.audio.exceptions;
+
+public class WavFileException extends Exception
+{
+    public WavFileException()
+    {
+        super();
+    }
+
+    public WavFileException(String message)
+    {
+        super(message);
+    }
+
+    public WavFileException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public WavFileException(Throwable cause)
+    {
+        super(cause);
+    }
+}
